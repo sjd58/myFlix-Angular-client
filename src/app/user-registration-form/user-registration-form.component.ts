2c52f0e1ed9a09f739ef426b2c6594a50fe1f3f5
sjd58 <dicksonsj0798@gmail.com>
2022-06-09 23:42:25 -0400

    Add profile-view component

--- src/app/user-registration-form/user-registration-form.component.ts
+++ src/app/user-registration-form/user-registration-form.component.ts
@@ -23,7 +23,6 @@ ngOnInit(): void {
 
 registerUser(): void {
     this.fetchApiData.userRegistration(this.userData).subscribe((result) => {
-  // Logic for a successful user registration goes here! (To be implemented)
      this.dialogRef.close(); // This will close the modal on success!
      console.log(result);
      this.snackBar.open(result, 'OK', {
