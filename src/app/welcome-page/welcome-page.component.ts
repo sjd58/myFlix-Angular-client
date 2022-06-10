19b9f63ce65e3af11cb9aceedf728d4ce290aec4
sjd58 <dicksonsj0798@gmail.com>
2022-06-10 17:41:19 -0400

    Change formatting for welcome-page component

--- src/app/welcome-page/welcome-page.component.ts
+++ src/app/welcome-page/welcome-page.component.ts
@@ -17,7 +17,7 @@ export class WelcomePageComponent implements OnInit {
       width: '280px'
     });
   }
-openUserLoginDialog(): void {
+  openUserLoginDialog(): void {
     this.dialog.open(UserLoginFormComponent, {
       width: '280px'
     });
