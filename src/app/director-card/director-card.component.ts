b0519a1aad8002444dba83c1edd26a1c7b668131
sjd58 <dicksonsj0798@gmail.com>
2022-06-10 17:40:03 -0400

    Got director-card component working

--- src/app/director-card/director-card.component.ts
+++ src/app/director-card/director-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-director-card',
@@ -7,7 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DirectorCardComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    @Inject(MAT_DIALOG_DATA)
+    public data: {
+      Name: string;
+      Bio: string;
+      Birth: string;
+      Death: string;
+    }
+  ) { }
 
   ngOnInit(): void {
   }
