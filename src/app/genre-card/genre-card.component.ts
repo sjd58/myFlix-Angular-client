0a04704d2922e1f221c88b5a2a589cc9e3888c79
sjd58 <dicksonsj0798@gmail.com>
2022-06-10 17:39:43 -0400

    Got genre-card component working

--- src/app/genre-card/genre-card.component.ts
+++ src/app/genre-card/genre-card.component.ts
@@ -1,13 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-genre-card',
   templateUrl: './genre-card.component.html',
   styleUrls: ['./genre-card.component.scss']
 })
+
 export class GenreCardComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    @Inject(MAT_DIALOG_DATA)
+    public data: {
+      Name: string;
+      Description: string;
+    }
+  ) { }
 
   ngOnInit(): void {
   }
