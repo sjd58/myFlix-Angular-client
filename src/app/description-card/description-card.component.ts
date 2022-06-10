8e05374b0487e58bbde14bb43834eeb5e632f3b4
sjd58 <dicksonsj0798@gmail.com>
2022-06-10 17:40:19 -0400

    Got description-card component working

--- src/app/description-card/description-card.component.ts
+++ src/app/description-card/description-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-description-card',
@@ -7,7 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DescriptionCardComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    @Inject(MAT_DIALOG_DATA)
+    public data: {
+      Title: string;
+      Description: string;
+    }
+  ) { }
 
   ngOnInit(): void {
   }
